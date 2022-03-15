7972761601dea2817f4014653c457dd5326a906b
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-03-15 21:17:17 +0100

    cleanup and extra checks

--- src/WebInteraction.py
+++ src/WebInteraction.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import lyricsgenius as lg #pip install git+https://github.com/johnwmillr/LyricsGenius.git
+import lyricsgenius as lg  # pip install git+https://github.com/johnwmillr/LyricsGenius.git
 import os
+
 
 class WebInteraction():
 
@@ -10,21 +11,24 @@ class WebInteraction():
         with open(token_path) as f:
             token = f.readlines()[0]
         self.genius = lg.Genius(token,
-                           skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"],
-                           remove_section_headers=True)
+                                skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"],
+                                remove_section_headers=True)
 
     def get_dataframe_from_url(self, url_link):
-        df = pd.read_html(url_link)
-        return df
+        try:
+            df = pd.read_html(url_link)
+            return df
+        except:
+            return []
 
     def lyrics_things(self, song_name, artist):
 
         lyrics = []
 
         try:
-            songs = self.genius.search_song(artist,song_name)
-            lyrics = songs.lyrics #search for song in songs, lyrics as string
-            lyrics = lyrics.split('\n',1)[1] #removing the head
+            songs = self.genius.search_song(artist, song_name)
+            lyrics = songs.lyrics  # search for song in songs, lyrics as string
+            lyrics = lyrics.split('\n', 1)[1]  # removing the head
 
         except:
             print(f"could not find lyrics to  {song_name}")
