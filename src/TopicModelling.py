fe5cf8a2925312d8101959e64a6311020fad0076
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-03-17 14:22:31 +0100

    Fix Errors in Topic Modelling

--- src/TopicModelling.py
+++ src/TopicModelling.py
@@ -1,4 +1,5 @@
 # data visualisation and manipulation
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,7 +10,7 @@ from sklearn.decomposition import LatentDirichletAllocation
 
 #configure
 # sets matplotlib to inline and displays graphs below the corressponding cell.
-%matplotlib inline
+#%matplotlib inline
 style.use('fivethirtyeight')
 sns.set(style='whitegrid',color_codes=True)
 
@@ -40,17 +41,17 @@ class TopicModelling():
         self.tm = TopicModelling()
         self.res_path = os.path.join("..", "res", )
 
-    def clean_text(text_str):   #df['texts_want_clean']=df['texts_want'].apply(clean_text)
+    def clean_text(self, text_str):   #df['texts_want_clean']=df['texts_want'].apply(clean_text)
         le = WordNetLemmatizer()
         word_tokens = word_tokenize(text_str)
         tokens = [le.lemmatize(w) for w in word_tokens if w not in stop_words and len(w) > 3]
         cleaned_text = " ".join(tokens)
         return cleaned_text
 
-    def make_corpus(df, column):
+    def make_corpus(self, df, column):
         clean_corpus = []
         for column in df:
-            clean_corpus.append(clean_text(df[column]).split())
+            clean_corpus.append(self.clean_text(df[column]).split())
         return clean_corpus
 
 
